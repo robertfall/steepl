bb0b0eb1fb40101272316e0c8ad6ecb06e54bc0c
Robert Fall <robert.fall@unboxedconsulting.com>
2013-02-19 18:27:31 +0200

    Force order for song in set list

--- app/controllers/song_sets_controller.rb
+++ app/controllers/song_sets_controller.rb
@@ -4,7 +4,7 @@ class SongSetsController < ApplicationController
   respond_to :html
 
   def index
-    @song_sets = SongSet.all
+    @song_sets = SongSet.order('created_at DESC')
     respond_with @song_sets
   end
 
--- app/models/song_sets_song.rb
+++ app/models/song_sets_song.rb
@@ -3,4 +3,9 @@ class SongSetsSong < ActiveRecord::Base
   belongs_to :song
   belongs_to :song_set
   scope :set_order, order('position_number')
+  after_create :determine_position_number
+
+  def determine_position_number
+    update_column(:position_number, song_set.song_sets_songs.count)
+  end
 end
