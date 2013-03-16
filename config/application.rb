b4e847a05f4ec3918d8bf4c91c7b66330ceb425d
Robert Fall <robert.fall@unboxedconsulting.com>
2013-03-16 21:54:03 +0200

    Add members.js to precompilation

--- config/application.rb
+++ config/application.rb
@@ -59,7 +59,7 @@ module Tvmethodist
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.precompile += ['uploader.js', 'sets.js']
+    config.assets.precompile += ['uploader.js', 'sets.js', 'members.js']
     config.assets.paths << "#{Rails.root}/vendor/assets/fonts"
 
     # Version of your assets, change this if you want to expire all your assets
