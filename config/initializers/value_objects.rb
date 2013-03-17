27684dd8c37967763639ea20f6142e70df735cbb
Robert Fall <robert.fall@unboxedconsulting.com>
2013-03-17 07:38:42 +0200

    Add Divorced as a status

--- config/initializers/value_objects.rb
+++ config/initializers/value_objects.rb
@@ -1,5 +1,5 @@
 CHURCH_SERVICES = %w{08h00 09h30 11h00 18h30}
-RELATIONSHIP_STATUSES = %w{Single Married Engaged In\ a\ Relationship Widowed Common\ Law}
+RELATIONSHIP_STATUSES = %w{Single Married Engaged In\ a\ Relationship Widowed Common\ Law Divorced}
 EMPLOYMENT_STATUSES = %W{Employed Unemployed Contract Retired Pre-School Primary\ School High\ School University\\College}
 CELL_GROUPS = ["Graham & Renee Lindsay", "Gilbert Perez", "Donald & Jill-Anne Gray", "Anne Neser", "Tom & Faith Adams",
                "M Labrum", "Andrew & Allison Holdsworth", "Alan Molyneux", "John ONeil", "Other"]
