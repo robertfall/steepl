714270560db1e019b6bcb1d2ce6aad6d23e858a2
Robert Fall <robert.fall@unboxedconsulting.com>
2013-03-17 10:27:09 +0200

    Add AWS initializer

--- app/helpers/upload_helper.rb
+++ app/helpers/upload_helper.rb
@@ -12,9 +12,9 @@ module UploadHelper
     def initialize(options)
       @options = options.reverse_merge(
         id: "fileupload",
-        aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-        aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-        bucket: ENV["AWS_S3_BUCKET"],
+        aws_access_key_id: AWS_ACCESS_KEY_ID,
+        aws_secret_access_key: AWS_SECRET_ACCESS_KEY,
+        bucket: AWS_S3_BUCKET,
         acl: "public-read",
         expiration: 10.hours.from_now,
         max_file_size: 50.megabytes,
